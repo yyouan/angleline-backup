1af9a2c934c8a70479a8ba35279954170e8b821f
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-11 03:08:47 +0800

    true-initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+    channeelId:"1580035125",
+    channelSecret:"837ed98e764e98030e2df9790645910f",
+    channnelAccessToken:"oFLYRsUpU+DvjkyL+7G1byY857G3tTascM5oU4J1bsyHsMuqQnydbBU864GRXqGDF7xFvwu64T/RrO64+cai0dY7Th5yno/goN9+dJVa4EsuQuQRhMxueOGdALIqZsNcaP5YOK+mY49B320jCS3BRQdB04t89/1O/w1cDnyilFU="
+});
+//update Token after 24 hr past
+
+//------------check conn-------------
+bot.on('message',(event)=>{
+    console.log(event);
+});
+
+const app = express(); //建立一個express 伺服器
+const linebotParser = bot.parser();
+app.post('/' , linebotParser); // POST 方法
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
