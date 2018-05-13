c3c53d6f714ebbd68e52c2f33bf7158e27bd8051
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-14 06:47:20 +0800

    JSON-all-15

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function linebotParser(req ,res){
                       'Authorization':'Client-ID ' +'8a38e76916407b6',
                       //'content-type':'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
                     },
-                    json:{ 'type' : 'file','image' : body }        
+                    json:{ 'type' : 'png' , 'image' : body }        
                   };
                   
                 request(imurg_options, function (error, response, body) {                    
