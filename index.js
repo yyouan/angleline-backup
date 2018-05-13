c495503a739d15cf60707a34b3a2e488b5ed89a8
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-14 05:43:15 +0800

    JSON-all-6

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var querystring = require('querystring');
 var request = require('request');
 var CHANNEL_ACCESS_TOKEN = 'BOpCS2JXlx/6DfqGmLVD9vU8FmjviF0TV/QJoLfkN0C465BHYiKtyfzP1Ov4wEIcF7xFvwu64T/RrO64+cai0dY7Th5yno/goN9+dJVa4EsLoNC5JV4mYF7ROws6Og6vfHByaSO/qQRZR8sy5Bz/twdB04t89/1O/w1cDnyilFU=';
 const line = require('@line/bot-sdk');
-var fs = require('fs');
 const client = new line.Client({
   channelAccessToken: 'CHANNEL_ACCESS_TOKEN'
 });
@@ -62,7 +61,7 @@ function linebotParser(req ,res){
         if (typeof replyToken === 'undefined') {
             return;
         }
-        
+        var imgurl="";
         if(post.events[0].message.type == 'image'){
             // Configure the request
             var options = {
@@ -78,13 +77,22 @@ function linebotParser(req ,res){
               if (!error && response.statusCode == 200) {
                   // Print out the response body
                   console.log(body);
-                  /**var filename ="temp/"+ post.events[0].message.id +".png";
-                  fs.writeFile(filename,body, function (err) {
-                    if (err)
-                        console.log(err);
-                    else
-                        console.log('Write operation complete.');
-                  });**/
+                  var imurg_options = {
+                    url: "https://api.imgur.com/3/image ",
+                    method: 'POST',
+                    headers: {
+                      'Content-Type':  'application/json', 
+                      'Authorization':'Client-ID ' +'8a38e76916407b6',
+                      'content-type':'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+                    },
+                    image:body        
+                  };
+                  
+                request(imurg_options, function (error, response, body) {                    
+                    if (error) throw error;
+                    console.log(body);
+                    imgurl=body.link;
+                });
               }else{
                 console.log("!!!!!error when recpt image!!!!!");                
               }
@@ -104,9 +112,11 @@ function linebotParser(req ,res){
                 'replyToken': replyToken,
                 'messages': [post.events[0].message]
             }
-
-          };
-          
+        };
+        if(post.events[0].message.type == 'image'){
+              options.json.messages[0].originalContentUrl=imgurl;
+              options.json.messages[0].previewImageUrl=imgurl;
+        }  
         request(options, function (error, response, body) {
             if (error) throw error;
             console.log(body);
