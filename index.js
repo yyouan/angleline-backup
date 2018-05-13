268e34382c24caebf7e713396b828b9108e21656
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-14 00:53:36 +0800

    new-world

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var linebot = require('linebot');
 var express = require('express');
-
-var bot = linebot({
-    channeelId:"1580035125",
-    channelSecret:"837ed98e764e98030e2df9790645910f",
-    channnelAccessToken:"BOpCS2JXlx/6DfqGmLVD9vU8FmjviF0TV/QJoLfkN0C465BHYiKtyfzP1Ov4wEIcF7xFvwu64T/RrO64+cai0dY7Th5yno/goN9+dJVa4EsLoNC5JV4mYF7ROws6Og6vfHByaSO/qQRZR8sy5Bz/twdB04t89/1O/w1cDnyilFU="
-});
-//update Token after 24 hr past
-
+var querystring = require('querystring');
+var http = require('http');
 
 //event will like:
 /**
@@ -28,59 +21,25 @@ var bot = linebot({
 }
  */
 
-//------------check conn-------------
-/**bot.on('message',(e)=>{
-    var CHANNEL_ACCESS_TOKEN = '你的 Channel access token';
-    var msg= JSON.parse(e.postData.contents);
-    console.log(msg);
-});**/
-//--------------deal with mess-------------
-/**bot.on('message', function(event) {
-    if (event.message.type = 'text') {
-      var msg = event.message.text;
-      event.reply(msg).then(function(data) {
-        // success 
-        console.log(msg);
-      }).catch(function(error) {
-        // error 
-        console.log('error');
-      });
-    }
-});**/
+//------------build TCP/IP-------------
+function linebotParser(req ,res){
+    // 定义了一个post变量，用于暂存请求体的信息
+    var post = '';     
+ 
+    // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
+    req.on('data', function(chunk){    
+        post += chunk;
+    });
+ 
+    // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
+    req.on('end', function(){    
+        post = querystring.parse(post);
+        console.log(post);
+    });
 
-bot.on('message',(e)=>{
-    var CHANNEL_ACCESS_TOKEN = 'BOpCS2JXlx/6DfqGmLVD9vU8FmjviF0TV/QJoLfkN0C465BHYiKtyfzP1Ov4wEIcF7xFvwu64T/RrO64+cai0dY7Th5yno/goN9+dJVa4EsLoNC5JV4mYF7ROws6Og6vfHByaSO/qQRZR8sy5Bz/twdB04t89/1O/w1cDnyilFU=';
-  var msg = JSON.parse(e.postData.contents);
-  console.log(msg);
-
-  // 取出 replayToken 和發送的訊息文字
-  var replyToken = msg.events[0].replyToken;
-  var userMessage = msg.events[0].message.text;
-
-  if (typeof replyToken === 'undefined') {
-    return;
-  }
-
-  var url = 'https://api.line.me/v2/bot/message/reply';
-  UrlFetchApp.fetch(url, {
-      'headers': {
-      'Content-Type': 'application/json; charset=UTF-8',
-      'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN,
-    },
-    'method': 'post',
-    'payload': JSON.stringify({
-      'replyToken': replyToken,
-      'messages': [{
-        'type': 'text',
-        'text': userMessage+' ( google )',
-      }],
-    }),
-  });
-
-},5000);
+}
 
 const app = express(); //建立一個express 伺服器
-const linebotParser = bot.parser();
 app.post('/' , linebotParser); // POST 方法
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
