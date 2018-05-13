47df17f8280a44b63f156141db25027f57c11a0f
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-13 21:47:05 +0800

    add space

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const linebotParser = bot.parser();
 app.post('/' , linebotParser); // POST 方法
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function() {
+var server = app.listen((process.env.PORT || 8080), function() {
     var port = server.address().port;
     console.log("App now running on port", port);
 });
