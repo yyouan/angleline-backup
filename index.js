f7dfab1cfae2fd7031b454052534b213bea71549
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-14 22:13:29 +0800

    JSON-all-33

--- index.js
+++ index.js
@@ -105,7 +105,9 @@ function linebotParser(req ,res){
                      { value: 'fs.createReadStream(body)',
                       options: 
                       { filename: __dirname+"/img.txt",
-                        contentType: null } } } };     
+                        contentType: null } } } 
+                    ,encoding : null  
+                   };     
                 
                   
                 request(imurg_options, function (error, response, body) {                    
@@ -147,6 +149,9 @@ function linebotParser(req ,res){
 
 const app = express(); //建立一個express 伺服器
 app.post('/' , linebotParser); // POST 方法**/
+app.get('/img.txt',(req,res)=>{
+    res.sendFile(__dirname+"/img.txt");
+});
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen((process.env.PORT || 8080), function() {
