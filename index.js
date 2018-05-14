27079960d47e4be37e0ae51cedca294392dc4b33
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-15 00:02:41 +0800

    JSON-all-38

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.post('/' , linebotParser); // POST 方法**/
 app.get('/img.jpg',(req,res)=>{
     //res.sendFile(__dirname+"/img.jpg");    
     res.writeHead(200, {'Content-Type': 'image/jpeg' });
-    res.end(Buffer.from(nwimg), 'binary');
+    res.end(nwimg, 'binary');
 });
 
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
