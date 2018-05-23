f7ea20cfd5204e0655e966176d4b6d80918539de
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-24 02:38:16 +0800

    login + test + hall

--- login.js
+++ login.js
@@ -1,46 +1,74 @@
 var express = require('express');
-var querystring = require('querystring');
 var request = require('request');
+const querystring = require('querystring');
 var CHANNEL_ACCESS_TOKEN = 'BOpCS2JXlx/6DfqGmLVD9vU8FmjviF0TV/QJoLfkN0C465BHYiKtyfzP1Ov4wEIcF7xFvwu64T/RrO64+cai0dY7Th5yno/goN9+dJVa4EsLoNC5JV4mYF7ROws6Og6vfHByaSO/qQRZR8sy5Bz/twdB04t89/1O/w1cDnyilFU=';
-var FormData = require('form-data');
 var fs = require('fs');
+const { Client } = require('pg');
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    //ssl: true,
+    });
 
 const app = express(); //建立一個express 伺服器
 app.post('/' , linebotParser); // POST 方法**/
-
-/**const message = {
-  type: 'text',
-  text: 'Hello World!'
-};
-
-client.replyMessage('<replyToken>', message)
-  .then(() => {
-    ...
-  })
-  .catch((err) => {
-    // error handling
-  });**/
-
-//event will like:
 /**
+ * expected result:
+ *  user:@ok     
+ *  bot:googleform
+ *  user:done
  * 
-{ type: 'message',
-  replyToken: 'xxxxxxx',
-  source: 
-    { userId: 'xxxxxxx',
-      type: 'user',
-      profile: [Function] },
-  timestamp: 1484472609833,
-  message: 
-    { type: 'text',
-      id: 'xxxxxxxxxx',
-      text: 'hihi',
-      content: [Function] },
-  reply: [Function] }
-}
  */
 
-//------------build TCP/IP-------------
+//SQL
+/**
+     angle_nickname |   angle_id    | master_nickname |  master_id   | department | student_id | 
+    ----------------+---------------+-----------------+------------------------------------------------
+    友安            | 0123456789012 | 主人             | 123456789012 | phys       |b05202030   |
+*/  
+
+//login message with recpt function:
+function record_angle_id(id){
+    psql("INSERT INTO ACCOUNT (angle_id) VALUES (\'"+ id +"\');");    
+}
+const googleform= {
+    "type": "template",
+    "altText": "This is a buttons template",
+    "template": {
+        "type": "buttons",        
+        "title": "填寫小主人資料",
+        "text": "讓小天使認識你",        
+        "actions": [
+            {
+              "type": "uri",
+              "label": "填寫",
+              "data": querystring.stringify(psy)
+            }
+        ]
+    }
+};
+function record_student_id(id,student_id){
+    psql("UPDATE ACCOUNT SET student_id=\'"+ student_id +"\' WHERE angle_id=\'" + id +"\';");
+}
+function passtopsql(id){
+
+}
+
+function psql(command){
+
+    var recpt =[];
+    client.connect();
+    console.log("(psql):" + command );
+    client.query(command, (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log( "(psql-query):"+ JSON.stringify(row));
+        recpt += row;
+    }
+    client.end();
+    });
+    return recpt;
+}
+
 function linebotParser(req ,res){
     //route
     var nwimg;
@@ -59,12 +87,34 @@ function linebotParser(req ,res){
         post = JSON.parse(post);
         console.log(post.events[0]);
         var replyToken = post.events[0].replyToken;
+        var posttype = post.events[0].type;
         /**var userMessage = post.events[0].message.text;
         console.log(replyToken);
         console.log(userMessage);**/
         if (typeof replyToken === 'undefined') {
             return;
         }
+
+        if (posttype == 'message'){
+            
+            if(post.events[0].message.type == 'text'){
+
+                if(post.events[0].message.text == "@ok"){
+                    record_angle_id(post.events[0].source.userId);
+                    var req = post.events[0].message;
+                    req.text ="請輸入你的學號";
+                    sendmessage(req);                    
+                }
+                if(post.events[0].message.text == "@done"){                    
+                    passtopsql(post.events[0].source.userId);
+                }
+                else{ //student id
+                    record_student_id( post.events[0].source.userId , post.events[0].message.text);
+                    sendmessage(googleform);
+                }
+            }
+        }
+
         //var imgurl="https://angleline.herokuapp.com/img.jpg";
         if(post.events[0].message.type == 'image'){
             //set adrr
@@ -118,7 +168,7 @@ function linebotParser(req ,res){
           sendmessage(nwimg);
         }
 
-        function sendmessage(recpt){
+        function sendmessage(recpt){ //recpt is message object
           var options = {
             url: "https://api.line.me/v2/bot/message/reply ",
             method: 'POST',
@@ -128,7 +178,7 @@ function linebotParser(req ,res){
             },
             json: {
                 'replyToken': replyToken,
-                'messages': [post.events[0].message]
+                'messages': [recpt]
             }
           };
           if(post.events[0].message.type == 'image'){
@@ -145,22 +195,8 @@ function linebotParser(req ,res){
               console.log("(line)");
               console.log(body);
           });
-          //create server
           
         }        
     });
 
 }
-
-
-
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen((process.env.PORT || 8080), function() {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-});
-//!!!
-
-
-
-
---
+++ text.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT angle_id FROM ACCOUNT WHERE angle_nickname=\'友安\';', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
