2b5731438098f9730d1979e4e6679d5c7d907bb3
yyouan <35065849+yyouan@users.noreply.github.com>
2018-05-24 03:19:14 +0800

    test-1

--- test.js
+++ test.js
@@ -16,12 +16,12 @@ const sqlclient2 = new Client({
                     //postgres://slyodakbbttdhs:968361395e30506915fb61997e79852b3a6f633c110d503411a4fdbc1aea8b9c@ec2-107-21-103-146.compute-1.amazonaws.com:5432/d8inumnm21ppuh
   ssl: true,
 });
-console.log(process.env.DATABASE_URL);
+//console.log(process.env.DATABASE_URL);
 sqlclient.connect();
 
-sqlclient.query('SELECT angle_id FROM ACCOUNT WHERE angle_nickname=\'友安\';', (err, res) => {
+sqlclient.query('SELECT angle_id FROM ACCOUNT WHERE student_id=\'b05202030\';', (err, res) => {
   if (err) throw err;
-  console.log(err);
+  //console.log(err);
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
