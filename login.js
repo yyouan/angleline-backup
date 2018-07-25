1033989216975189dc4e06d1aa14d81424a818ea
yyouan <35065849+yyouan@users.noreply.github.com>
2018-07-25 22:38:49 +0800

    test post querystring

--- login.js
+++ login.js
@@ -370,7 +370,7 @@ function FormReceiver(req,res){
     // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
     req.on('end', function(){
         post = querystring.parse(post);    
-        console.log("-----------post:"+ String(post));
+        console.log(post);
         psql("INSERT INTO ACCOUNT (email) VALUES (\'"+ post.email +"\');").then(
             res =>{
                 psql("UPDATE ACCOUNT SET angle_nickname=\'"+ post.nickname +"\' WHERE email=\'" + post.email +"\';");
@@ -404,7 +404,7 @@ function imgReceiver(req,res){
     // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
     req.on('end', function(){
         post = querystring.parse(post);    
-        console.log("-----------post:" + String(post));
+        console.log(post);
         psql("UPDATE ACCOUNT SET times=\'"+ post.url +"\' WHERE email=\'" + post.email +"\';");        
     });
 }
