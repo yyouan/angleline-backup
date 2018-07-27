143fe903d405ceb3a6654fccfc86df12cb5540bc
yyouan <35065849+yyouan@users.noreply.github.com>
2018-07-27 09:11:15 +0800

    test past intro add from account

--- exchange_id.js
+++ exchange_id.js
@@ -185,7 +185,8 @@ function psql(command){
         
             request(options, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    
+                    body = JSON.parse(body);
+                    console.log(body);
                     let bubble_to_master ={
                         "type": "bubble",
                         "header": {
