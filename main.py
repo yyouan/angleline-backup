944a9dfb32bc0660331201aecaa422aa340f9972
yyouan <b05202030@ntu.edu.tw>
2018-08-06 08:56:49 +0800

    ab

--- main.py
+++ main.py
@@ -56,8 +56,9 @@ if name =="show":
     print(os.system("heroku pg:psql --app angleline < main.sql"))
     print(os.system("notepad list.txt"))
 
-if name =="console":    
-    webbrowser.open_new("https://dashboard.heroku.com/apps/angleline/logs")
+if name =="console":
+    webbrowser.open("https://www.google.com")    
+    webbrowser.open_new_tab("https://dashboard.heroku.com/apps/angleline/logs")
     webbrowser.open_new_tab("https://dashboard.heroku.com/apps/angleline-master/logs")
     webbrowser.open_new_tab("https://dashboard.heroku.com/apps/angleline-hall/logs")
     webbrowser.open_new_tab("https://dashboard.heroku.com/apps/informationdesk/logs")
