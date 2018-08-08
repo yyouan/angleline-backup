e5285096aa2754ec5fbb9e5d3285c55958b8dab3
yyouan <35065849+yyouan@users.noreply.github.com>
2018-08-08 13:53:56 +0800

    login

--- info.js
+++ info.js
@@ -339,7 +339,7 @@ function chatParser(req ,res){
 
                             text.text = "!!!!抵達目標，恭喜答對!!!!"                           
 
-                            psql("UPDATE ACCOUNT SET score="+ String(res[0].score+20) +" WHERE angle_id=\'" + res[0].angle_id +"\';");
+                            psql("UPDATE ACCOUNT SET score="+ String(res[0].score+10) +" WHERE angle_id=\'" + res[0].angle_id +"\';");
                             let msg =[]
                             if(res[0].location_count < game_item.locationproblem.length){
 
