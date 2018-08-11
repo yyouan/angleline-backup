4d45b23c68437de04d6509e928db60740195d478
yyouan <b05202030@ntu.edu.tw>
2018-08-11 13:39:19 +0800

    change key

--- past_intro_master.js
+++ past_intro_master.js
@@ -2,12 +2,13 @@ var express = require('express');
 var request = require('request');
 const querystring = require('querystring');
 const game_item = require('./game_item.js');
+const token = require('./token.js');
 const [AngleToken,MasterToken,HallToken,InfoToken] = [
-    'JeHZW0fzS1rQX9yHvGRz0ZZqC+ENFDhsf/30grCHGk80MBiNjqDz76oj+ETgTnAXPjFCp/P/1EzYYbq4Ptz6U8tLCUxBHBlLeH4iozbORQOq1zYSc2cKosq8esu3/ttrZdeRRo0wsBoWI4gjTeEjuQdB04t89/1O/w1cDnyilFU=', 
-    'BOpCS2JXlx/6DfqGmLVD9vU8FmjviF0TV/QJoLfkN0C465BHYiKtyfzP1Ov4wEIcF7xFvwu64T/RrO64+cai0dY7Th5yno/goN9+dJVa4EsLoNC5JV4mYF7ROws6Og6vfHByaSO/qQRZR8sy5Bz/twdB04t89/1O/w1cDnyilFU=',  
-    'chRfdlc9nHQJyi8BLGXxExjrfNoGBMfH8DPqevbDaPYsgvP1WsZ8Aqi17HRS4dpfjtSLU5QD3G6b/RjZ4GflCh4N/hIhqWhBPPUJ56dhzxAfqRtgSPYadNTsTbcV/Hm1l4YUiJHYoDqaWO2o2qY/yAdB04t89/1O/w1cDnyilFU=',
-    'bE7q3TnTG/MO9rE+0sME3betLgGFgqUpYCOv0OrmW/Uefjldl9a5am6xNyC0VRcnL87qKx1GMoPzGLKQDX/PRiERLTdZ2uIf5txK+1+JhIFsSIGwI00lGGaGavvCzkyKfy5A6QrqWZdfeu0J08SJDAdB04t89/1O/w1cDnyilFU='
-  ]
+    token.AngleToken,
+    token.MasterToken,
+    token.HallToken,
+    token.InfoToken
+]
 var CHANNEL_ACCESS_TOKEN = MasterToken;
 const modetype =["angle_id","master_id"];
 const mode = modetype[1];
