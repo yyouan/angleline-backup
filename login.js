6b12f8245cb787aac96eeec5b74746f5878f2c5c
yyouan <b05202030@ntu.edu.tw>
2018-08-20 03:11:11 +0800

    h12 fix

--- login.js
+++ login.js
@@ -612,7 +612,7 @@ function loginParser(req ,res){
           
         }        
     });
-
+    res.end("OK")
 }
 //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
 var server = app.listen((process.env.PORT || 8080), function() {
