b8ad3964ea171547adcfc5a943ada3b783289788
yyouan <b05202030@ntu.edu.tw>
2018-09-21 12:41:28 +0800

    log.py

--- log.py
+++ log.py
@@ -5,6 +5,9 @@ import webbrowser
 import time
 
 while True:
-    os.system("heroku logs -n 1500 >> logs.txt")
+    os.system("heroku logs -n 1500 --app angleline-hall >> ../hall_logs.txt")
+    os.system("heroku logs -n 1500 --app angleline >> ../angle_logs.txt")
+    os.system("heroku logs -n 1500 --app angleline-master >> ../master_logs.txt")
+    os.system("heroku logs -n 1500 --app informationdesk >> ../info_logs.txt")
     print('Running on background ...')
     time.sleep(60*10)
