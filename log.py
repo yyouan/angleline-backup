c0232e90ba8f34f13c99a1207a6cf49ecb89e49c
yyouan <b05202030@ntu.edu.tw>
2018-09-22 10:04:15 +0800

    login

--- log.py
+++ log.py
@@ -15,5 +15,5 @@ while True:
     os.system("heroku logs -n 1500 --app angleline >> ../angle_logs.txt")
     os.system("heroku logs -n 1500 --app angleline-master >> ../master_logs.txt")
     os.system("heroku logs -n 1500 --app informationdesk >> ../info_logs.txt")
-    print('Running on background ...')
+    print('Running on background ...',time.asctime( time.localtime(time.time()) ))
     time.sleep(60*10)
